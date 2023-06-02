4bf90f77430e1cd754f85e27a6b8f0c80fa5ce47
detteiu8383 <ysdr83@gmail.com>
2023-06-03 02:58:22 +0900

    :wrench: add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+/** @type {import("eslint").ESLint.ConfigData} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "plugin:solid/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: ["./tsconfig.base.json", "./tsconfig.eslint.json"],
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["solid"],
+  rules: {},
+  ignorePatterns: ["dist", "node_modules"],
+};
