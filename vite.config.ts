92767791fe0bbdbe45f6be85e1efe1d8749fa6c4
detteiu8383 <ysdr83@gmail.com>
2023-06-10 16:57:16 +0900

    :fire: remove solid-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { macaronVitePlugin } from "@macaron-css/vite";
 import mdx from "@mdx-js/rollup";
-import devtools from "solid-devtools/vite";
 import solid from "solid-start/vite";
 import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    devtools(),
     mdx({
       jsxImportSource: "solid-jsx",
     }),
